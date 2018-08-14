591f8c116fbb4a01fedc66dfa8d12ebafa4c4fa2
Andrei Egorov <andreyegorov1992@gmail.com>
2018-08-14 13:02:03 -0700

    Add compression solution

--- __tests__/string-compression-test.js
+++ __tests__/string-compression-test.js
@@ -1,47 +1,110 @@
 
 const compression = require('../_lib/string-compression.js')
 
-describe("compression", () => {
-    describe("when the string contains numeric characters", () => {
-        it("should remove numbers from the string ", () => {
-            const result = compression("1aab3bcc")
-            expect(result).toEqual("aabbcc")
-        })
-    } )
-} )
+// describe("compression", () => {
+//     describe("when the string contains numeric characters", () => {
+//         it("should remove numbers from the string ", () => {
+//             const result = compression("1aab3bcc")
+//             expect(result).toEqual("aabbcc")
+//         })
+//     } )
+// } )
 
-describe("compression", () => {
-    describe("when there is a string of letters", () => {
-        it("should return a compressed string", () => {
-            const result = compression("aabbcc")
-            expect(result).toEqual("a1b1c1")
+// describe("compression", () => {
+//     describe("when there is a string of letters", () => {
+//         it("should return a compressed string", () => {
+//             const result = compression("aabbcc")
+//             expect(result).toEqual("a1b1c1")
+//         })
+//     })
+// })
+
+// describe("compresion", () => {
+//     describe("when there is a string with spaces", () => {
+//         it("should return a compressed string with no spaces", () => {
+//             const result =  compression("bboc rroods ddlla")
+//             expect (result).toEqual("b2o1c1r2o2d1s1d2l2a1")
+//         })
+//     })
+// } )
+
+// describe("compresion", () => {
+//     describe("when there are punctuation marks in a string", () => {
+//         it("should return a compressed string with no punctuation", () => {
+//             const result =  compression("bboc!rroods?ddlla,")
+//             expect (result).toEqual("b2o1c1r2o2d1s1d2l2a1")
+//         })
+//     })
+// } )
+// describe("compresion", () => {
+//     describe("when there are uppercase characters ", () => {
+//         it("should return a compressed string with no punctuation", () => {
+//             const result =  compression("bboc!rroods?ddlla,")
+//             expect (result).toEqual("b2o1c1r2o2d1s1d2l2a1")
+//         })
+//     })
+// } )
+
+
+
+
+
+
+
+
+// ----------------------
+describe("compresion", () => {
+    describe("when string contains numbers", () => {
+        it("should throw an error", () => {
+            const result =  compression("bb1cc2")
+            expect (result).toEqual("b2c2")
         })
     })
-})
+} )
+
 
 describe("compresion", () => {
-    describe("when there is a string with spaces", () => {
-        it("should return a compressed string with no spaces", () => {
-            const result =  compression("bboc rroods ddlla")
-            expect (result).toEqual("b2o1c1r2o2d1s1d2l2a1")
+    describe("when string contains repeated characters", () => {
+        it("should count characters in place", () => {
+            const result =  compression("aabccdaa")
+            expect (result).toEqual("a2b1c2d1a2")
         })
     })
 } )
 
 describe("compresion", () => {
-    describe("when there are punctuation marks in a string", () => {
-        it("should return a compressed string with no punctuation", () => {
-            const result =  compression("bboc!rroods?ddlla,")
-            expect (result).toEqual("b2o1c1r2o2d1s1d2l2a1")
-        })
-    })
-} )
-describe("compresion", () => {
-    describe("when there are uppercase characters ", () => {
-        it("should return a compressed string with no punctuation", () => {
-            const result =  compression("bboc!rroods?ddlla,")
-            expect (result).toEqual("b2o1c1r2o2d1s1d2l2a1")
+    describe("when asked to compres not a string", () => {
+        it("should error", () => {
+            const result =  compression("420")
+            expect (result).toEqual(0)
         })
     })
 } )
 
+describe("compresion", () => {
+    describe("when string contains non-letter chars", () => {
+        it("should ignore those, but count the letters", () => {
+            const result =  compression("Hello World!!!")
+            expect (result).toEqual("H1e1l2o1W1o1r1l1d1")
+        })
+    })
+} )
+
+describe("compresion", () => {
+    describe("when is called without an argument", () => {
+        it("should throw an error", () => {
+            const result =  compression()
+            expect (result).toEqual(0)
+        })
+    })
+} )
+
+
+describe("compresion", () => {
+    describe("when is called with an empty string", () => {
+        it("should throw an error", () => {
+            const result =  compression("")
+            expect (result).toEqual(0)
+        })
+    })
+} )
--- _lib/string-compression.js
+++ _lib/string-compression.js
@@ -1,9 +1,67 @@
-module.exports = (string)=>{
-    // const char = string.map(char => char)  
-    // string.split("").filter(char => typeof (char) === "string").join("")
-// return string.split("").filter(char => typeof (char) === "string").join("")
+module.exports = string => {
+ 
+    if(typeof string !== "string" || string === "" || !string.match(/[a-z]/ ))
+    return 0
 
-}
+   let cleanStr = string.replace(/[\W_]/g, '')
+   var count = 0 
+   var newString = ''
+
+    for(let i=0; i <cleanStr.length; i++){
+       count++
+        if(cleanStr[i] != cleanStr[i+1]){
+            newString += cleanStr[i] + count
+            count = 0
+
+        }
+        
+    }
+    
+    return newString
+};
 
 
-// .replace(/[0-9]/g, '').join('')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ------------------------------------------
+// module.exports = string => {
+//     //    const cleanString = string.split("").filter(char => typeof(char) === "string")
+//     // const cleanString = string.split("").map(char => char)
+  
+//     // return cleanString
+//     let output = "";
+//     let count = 0;
+  
+//     if (typeof string !== "string" || string === "") return 0;
+  
+//     for (let i = 0; i < string.length; i++) {
+//       count++;
+//       if (!string[i].match(/[a-z]/i)) return 0;
+  
+//       if (string[i] != string[i + 1]) {
+//         output += string[i] + count;
+//         count = 0;
+//       }
+//     }
+//     return output;
+//   };
+  
