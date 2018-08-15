d4dac68de936532fbc44666d4fc5dc2db68afda8
Andrei Egorov <andreyegorov1992@gmail.com>
2018-08-14 23:17:04 -0700

    Add solution function to unique-character tests

--- _lib/unique-character.js
+++ _lib/unique-character.js
@@ -1,8 +1,20 @@
 module.exports = string => {
- if(typeof string !== "string") return 0
-   
+  if (typeof string !== "string" || !string || !string.length) return 0;
+  if (string.match(/^(.)\1{1,}$/)) return undefined;
+  if (string.match(/[0-9]/)) return 0;
+
+  let myArray = string.split("");
+  let arrayLength = myArray.length;
+  let repeatValues = [];
+  let uniqueValues = [];
+
+  for (let i = 0; i < arrayLength; i++) {
+    var shifted = myArray.shift(i);
+    if (myArray.includes(shifted) || repeatValues.includes(shifted)) {
+      repeatValues.push(shifted);
+    } else {
+      uniqueValues.push(shifted);
     }
-    
-
-
-
+  }
+  return uniqueValues.join("");
+};
