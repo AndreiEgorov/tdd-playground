00be3dd3da8585a1798d814811505bdaff6a2e81
Andrei Egorov <andreyegorov1992@gmail.com>
2018-08-15 00:38:28 -0700

    Add a function and pass min-integer-test

--- __tests__/min-integer-test.js
+++ __tests__/min-integer-test.js
@@ -1,56 +1,44 @@
+const mininteger = require("../_lib/min-integer.js");
 
-const mininteger = require('../_lib/min-integer.js')
+describe("mininteger", () => {
+  describe("when value is not an array", () => {
+    it("it should throw an error", () => {
+      const result = mininteger("hello");
+      expect(result).toEqual(0);
+    });
+  });
+});
 
+describe("mininteger", () => {
+  describe("when array contains non-numeric characters ", () => {
+    it("it should throw an error", () => {
+      const result = mininteger([1, 4, 2, { greeting: "hello" }, 6]);
+      expect(result).toEqual(0);
+    });
+  });
+});
 
-describe("mininteger",() => {
-    describe("when value is not an array", ()=>{
-        it("it should throw an error", () => {
-            const result = mininteger("hello")
-            expect(result).toEqual(0)
-        })
-    })
-})
-
-describe("mininteger",() => {
-    describe("when array contains non-numeric characters ", ()=>{
-        it("it should throw an error", () => {
-            const result = mininteger([1,4,2, "hi", 6])
-            expect(result).toEqual(0)
-        })
-    })
-})
-
-describe("mininteger",() => {
-    describe("when array contains non-numeric characters ", ()=>{
-        it("it should throw an error", () => {
-            const result = mininteger([1,4,2, "hi", 6])
-            expect(result).toEqual(0)
-        })
-    })
-})
-
-describe("mininteger",() => {
-    describe("when array does not have a missing value", ()=>{
-        it("it should add a value to the tail of an array", () => {
-            const result = mininteger([1,2,3,4])
-            expect(result).toEqual(5)
-        })
-    })
-})
-describe("mininteger",() => {
-    describe("when one or more values in the array is < 0", ()=>{
-        it("it should throw an error", () => {
-            const result = mininteger([1,2,-3,4])
-            expect(result).toEqual(0)
-        })
-    })
-})
-describe("mininteger",() => {
-    describe("when there is a missing positive integer", ()=>{
-        it("it should return a minimal positive integer", () => {
-            const result = mininteger([1,1,2,4])
-            expect(result).toEqual(3)
-        })
-    })
-})
-
+describe("mininteger", () => {
+  describe("when array does not have a missing value", () => {
+    it("it should add a value to the tail of an array", () => {
+      const result = mininteger([1, 2, 3, 4]);
+      expect(result).toEqual(5);
+    });
+  });
+});
+describe("mininteger", () => {
+  describe("when one or more values in the array is =< 0", () => {
+    it("it should throw an error", () => {
+      const result = mininteger([1, 2, -3, 4]);
+      expect(result).toEqual(0);
+    });
+  });
+});
+describe("mininteger", () => {
+  describe("when there is a missing positive integer", () => {
+    it("it should return a minimal positive integer", () => {
+      const result = mininteger([1, 1, 4, 2]);
+      expect(result).toEqual(3);
+    });
+  });
+});
--- _lib/min-integer.js
+++ _lib/min-integer.js
@@ -1,3 +1,18 @@
- module.exports = (array) => {
-return
-}
+module.exports = array => {
+  if (!Array.isArray(array)) return 0;
+  if (
+    array.some(value => value <= 0) ||
+    array.some(value => typeof value !== "number")
+  )
+    return 0;
+
+  adjustedArray = array.sort();
+
+  for (let i = 0; i < array.length; i++) {
+    let firstNum = adjustedArray[i];
+    let secondNum = adjustedArray[i + 1];
+    if (secondNum - firstNum > 1 || secondNum == undefined) {
+      return firstNum + 1;
+    }
+  }
+};
