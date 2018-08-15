c7ed1fa44144b674bb27cefb8ba6b8b12a2da497
Andrei Egorov <andreyegorov1992@gmail.com>
2018-08-15 09:46:20 -0700

    Add tests for one-way function

---
+++ __tests__/one-away-test.js
@@ -0,0 +1,45 @@
+const oneAway = require("../_lib/one-away.js")
+
+describe("oneAway", () =>{
+    describe("when at least one of the inputs is not a string", () => {
+        it("should throw an error", () =>{
+            const result = oneAway("oneword", 123);
+            expect(result).toEqual(0)
+        })
+    } ) 
+})
+
+
+describe("oneAway", () =>{
+    describe("when there are fewer than two strings", () => {
+        it("should throw an error", () =>{
+            const result = oneAway("oneword");
+            expect(result).toEqual(0)
+        })
+    } ) 
+})
+
+describe("oneAway", () => {
+    describe("when there are more than two strings", ()=>{
+        it("should throw an error", ()=>{
+            const result = oneAway("oneword","twoword", "threeword")
+            expect(result).toEqual(0)
+        })
+    })
+})
+describe("oneAway", () => {
+    describe("when the words are more than one edit apart", ()=>{
+        it("should return false", ()=>{
+            const result = oneAway("car", "shar")
+            expect(result).toEqual(false)
+        })
+    })
+})
+describe("oneAway", () => {
+    describe("when the words are one edit apart", ()=>{
+        it("should return true", ()=>{
+            const result = oneAway("car", "bar")
+            expect(result).toEqual(true)
+        })
+    })
+})
---
+++ _lib/one-away.js
@@ -0,0 +1,3 @@
+module.exports = (strone, strtwo) => {
+    return
+}
